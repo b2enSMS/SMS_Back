ef192cde95497b180e569ee7d169cd24ad73501f
JInwoo Kim <ypk02007@naver.com>
2019-12-30 10:31:09 +0900

    image download에서 header의 "attatchment;" 제거

--- src/main/java/com/b2en/sms/controller/file/ScanController.java
+++ src/main/java/com/b2en/sms/controller/file/ScanController.java
@@ -83,7 +83,7 @@ public class ScanController {
 		
 		return ResponseEntity.ok()
 				.contentType(MediaType.parseMediaType(contentType))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.header(HttpHeaders.CONTENT_DISPOSITION, "filename=\"" + resource.getFilename() + "\"")
 				.body(resource);
     }
     
