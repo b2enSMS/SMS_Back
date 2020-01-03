44d156db1c82bca3816a648f6c1601de4c73c51b
JInwoo Kim <ypk02007@naver.com>
2020-01-03 09:49:52 +0900

    tempvercrud

--- src/main/java/com/b2en/sms/repo/TempVerHistRepository.java
+++ src/main/java/com/b2en/sms/repo/TempVerHistRepository.java
@@ -2,6 +2,8 @@ package com.b2en.sms.repo;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,6 +13,9 @@ import com.b2en.sms.entity.pk.TempVerHistPK;
 public interface TempVerHistRepository extends JpaRepository<TempVerHist, TempVerHistPK> {
 	
 	public List<TempVerHist> findByTempVerHistPKTempVerId(int tempVerId);
+	
+	@Transactional
+	void deleteByTempVerHistPKTempVerId(int tempVerId);
 	
 	@Query(value="SELECT max(temp_ver_hist_seq) FROM temp_ver_hist", nativeQuery = true)
 	Integer findMaxTempVerHistSeq();
