dee21f4d18fee30c74ce796903d4155ed6b77bfa
JInwoo Kim <ypk02007@naver.com>
2020-01-17 13:07:32 +0900

    lcnsNo 자동생성

---
+++ src/main/java/com/b2en/sms/dto/GeneratingLcnsNo.java
@@ -0,0 +1,17 @@
+package com.b2en.sms.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.Data;
+
+@Data
+public class GeneratingLcnsNo {
+	
+	@NotBlank()
+	private String prdtNm;
+	
+	@NotBlank()
+	@Pattern(regexp="^([12]\\d{3}-(0[1-9]|1[0-2]|[1-9])-(0[1-9]|[12]\\d|3[01]))$")
+	private String installDt;
+}
